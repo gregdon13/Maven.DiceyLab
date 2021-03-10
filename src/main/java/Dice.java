aa9781e2def381df499102c5ec216cc8ef154b4f
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-10 14:53:27 -0500

    Dice is semi functioning, and tests are passing for now

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+
 public class Dice {
+    private Integer sumOfDice = 0;
+    private Integer numOfDice;
+
+    public Dice(Integer numOfDice) {
+        this.numOfDice = numOfDice;
+    }
+
+    public ArrayList<Integer> rollDice() {
+        ArrayList<Integer> diceRolls = new ArrayList<Integer>();
+        for (int i = 0; i < numOfDice; i++) {
+            diceRolls.add(getIndividualRoll());
+        }
+        return diceRolls;
+    }
+
+    public void sumOfRolls(ArrayList<Integer> diceRolled) {
+        diceRolled = rollDice();
+        Integer[] diceArr = diceRolled.toArray(new Integer[0]);
+        for (int i = 0; i < diceArr.length; i++) {
+            sumOfDice += diceArr[i];
+        }
+    }
+
+    public int getIndividualRoll() {
+        int ranNum = (int) (Math.random() * 6) + 1;
+        return ranNum;
+    }
 
 
+    public Integer getSum() {
+        return sumOfDice;
+    }
+
+
+    public Integer getNumberOfDice() {
+        return this.numOfDice;
+    }
 }
