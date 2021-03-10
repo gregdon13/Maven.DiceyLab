5d9903086e0a007753cba7e3a032a2014dd1e507
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-10 14:54:12 -0500

    had to add Test directory

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,35 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest {
+    //getNumberOfDice
+    @Test
+    public void dieNumber() {
+        Dice name = new Dice(1);
+        Integer expected = 1;
+
+        Assert.assertEquals(expected, name.getNumberOfDice());
+    }
+
+    //getIndividualRoll
+    @Test
+    public void indRollTest() {
+        Dice testRoll = new Dice(1);
+        Integer x = testRoll.getIndividualRoll();
+
+        Assert.assertTrue(x >= 1 && x < 7);
+    }
+
+    //getSum
+    @Test
+    public void getSumTest() {
+        Dice testRoll = new Dice (2);
+        testRoll.sumOfRolls(testRoll.rollDice());
+        Integer x = testRoll.getSum();
+
+        //System.out.print(x);
+        Assert.assertTrue(x > 1 && x < 13);
+    }
+
+}
